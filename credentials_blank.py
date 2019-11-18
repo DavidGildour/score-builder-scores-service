69af4c2f7e367ada6a7682bc4b1651d134830fc4
DavidGildour <davidgildour@gmail.com>
2019-11-18 13:27:16 +0100

    added blank credentials file

---
+++ credentials_blank.py
@@ -0,0 +1,15 @@
+dbURI_dev = {
+    'user': None,
+    'password': None,
+    'host': 'score-builder_scores-db_1',
+    'port': '5432',
+    'db': 'users_dev'
+}
+
+dbURI_test = {
+    'user': None,
+    'password': None,
+    'host': 'localhost',
+    'port': '5432',
+    'db': 'users_test'
+}
